4afeb0e5b3fe48f06012a412e63974b2afdcb5f9
Zelone <leandro.zelone@hotmail.com>
2026-04-21 11:24:10 -0300

    mod 03

---
+++ modulo 02/mod 03/src/config.ts
@@ -0,0 +1,34 @@
+export type ModelConfig = {
+  apiKey: string;
+  httpReferer: string;
+  xTitle: string;
+
+  provider: {
+    sort: {
+      by: string;
+      partition: string;
+    };
+  };
+
+  models: string[];
+  temperature: number;
+};
+
+console.assert(process.env.OPENROUTER_API_KEY, 'OPENROUTER_API_KEY is not set in environment variables');
+
+export const config: ModelConfig = {
+  apiKey: process.env.OPENROUTER_API_KEY!,
+  httpReferer: '',
+  xTitle: 'IA Devs - Prompt Chaining Article Generator',
+  models: [
+    //https://openrouter.ai/models?max_price=0&fmt=cards&supported_parameters=response_format - lista de provedores que permitem response format
+    'openrouter/elephant-alpha',
+  ],
+  provider: {
+    sort: {
+      by: 'throughput', // Route to model with highest throughput (fastest response)
+      partition: 'none',
+    },
+  },
+  temperature: 0.7,
+};
